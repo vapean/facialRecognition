2690d49a268a22349907c711979389b40d12d47f
Antonio Vallejo <antonio.vapei@gmail.com>
2020-04-19 19:49:31 +0200

    añadida edad y genero

--- main.js
+++ main.js
@@ -38,15 +38,25 @@ video.addEventListener('play', async () => {
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
 
+
+
+
     setInterval(async () => {
         // detección de la posicion de la cara y todas las demás detecciones
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender().withFaceDescriptors();
         // console.log(detections);
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         faceapi.draw.drawDetections(canvas, resizedDetections)
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        console.log(detections)
+        resizedDetections.forEach(detection => {
+            const box = detection.detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
+            drawBox.draw(canvas)
+        })
+
 
 
     }, 100);
